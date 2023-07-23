c3981e731f1c074273488f1b8fd1a9b7b67aa635
PAULO ROBERTO DE LIMA <138160699+PauloRobertoDelima-dev@users.noreply.github.com>
2023-07-23 08:33:08 -0300

    adding files today
    continuing the project

--- App.jsx
+++ App.jsx
@@ -1,22 +1,31 @@
 
-import { Post } from './Post';
 import { Header } from './components/header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
 
 import './global.css';
+
 
 export function App() {
 
   return (
     <div>
       <Header />
-      <Post
-       author="Paulo Roberto De Lima"
-       content="Se não foi sábio para apreender com os erros dos outros, pelo menos seja inteligente e aprenda com os seus própios.!"
-      />
-      <Post
-       author="Pablo Marçal"
-       content="Erre rápido!"
-      />
+       <div className={styles.wrapper}>
+         <Sidebar />
+         <main>
+           <Post
+           author="Paulo Roberto De Lima"
+           content="Se não foi sábio para apreender com os erros dos outros, pelo menos seja inteligente e aprenda com os seus própios.!"
+           />
+           <Post
+           author="Pablo Marçal"
+           content="Erre rápido!"
+           />
+         </main>
+      </div>
     </div>
   )
 }
